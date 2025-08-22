5b63cc28cc8a57b7e26e20f74b732e66d3ff5e70
SeguNagendra <nagi2426@gmail.com>
2025-08-23 00:06:01 +0530

    sample website code

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, Welcome to Sample Web App!</h1>");
+        out.println("</body></html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Sample Web App</title>
+</head>
+<body>
+    <h2>Welcome to the Sample Web Application!</h2>
+    <p><a href="hello">Click here</a> to call the servlet.</p>
+</body>
+</html>
